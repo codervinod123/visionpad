562eb4e66ead48e5c5659561f6c65874414a7a0a
Vinod Prajapati <vinodpr737947@gmail.com>
2025-01-15 17:51:01 +0530

    common database added

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import {CreateUserSchema} from "@repo/common/types";
 import {PORT} from "@repo/backend-common/config"
+import {prisma} from "@repo/db/prisma"
 
 const app = express();
-app.listen(PORT,()=>{
+app.listen(PORT,async()=>{
    CreateUserSchema();
+   await prisma.user.create({data:{name:"vinit", email:"vinit@gmail.com",password:"vinit@123"}});
    console.log(`Server is running on PORT ${PORT}`);
 })
 
